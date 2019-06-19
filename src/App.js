4f13a630ba51f3d51b8827ff79e4965673a96d9b
daniel <danielzhengxian21@gmail.com>
2019-06-19 19:36:37 +0800

    state and props

--- src/App.js
+++ src/App.js
@@ -1,13 +1,22 @@
 import React from 'react';
 import './App.css';
+import Todos from './components/Todos'
 
 class App extends React.Component {
+  state = {
+    todos: [
+      {id:1, title: 'take out trash', completed: false},
+      {id:2, title: 'dinner with wife', completed: false},
+      {id:3, title: 'meeting with boss', completed: false},
+    ]
+  }
+
   render() {
     return (
-
       <div className="App">
-        <h1>Hello</h1>
-    </div>
+        <h1>this is App</h1>
+        <Todos todos={this.state.todos} />
+      </div>
     )
   }
 }
---
+++ src/components/Todos.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import TodoItem from './TodoItem'
+class Todos extends React.Component {
+  render() {
+    return (
+        this.props.todos.map(todo=>{
+            return <TodoItem todo={todo}/>
+        })
+    )
+  }
+}
+
+export default Todos;
