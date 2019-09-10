808e39b680e222440566532babc65b19bc9b1df5
Daniel Kwok <danielkwok@gpsfleet.my>
2019-09-10 15:56:36 +0800

    redux implemented

---
+++ src/redux/actions/index.js
@@ -0,0 +1,9 @@
+import ADD_ARTICLE from './actionTypes'
+
+export function addArticle(payload){
+    return {
+        type: ADD_ARTICLE,
+        payload
+    }
+}
+
---
+++ src/redux/reducers/index.js
@@ -0,0 +1,28 @@
+// import a bunch of constants
+import ADD_ARTICLE from '../actions/actionTypes'
+
+// initial state of the store
+const initialState = {
+    articles: []
+}
+
+// the only reducer we have now, rootReducer
+function rootReducer(state = initialState, action){
+    switch(action.type){
+        case ADD_ARTICLE:
+            // makes a copy of the store
+            const newState = Object.assign({}, state)
+
+            // make changes to the newState
+            newState.articles.push(action.payload)
+
+            // returns the newState
+            return newState
+        default:
+            return state
+    }    
+}
+
+export default rootReducer
+
+
