c64eec402dbedab31f4a0e26b3e32c55bd9d6274
Daniel Kwok <danielkwok@gpsfleet.my>
2019-11-13 10:29:53 +0800

    firstcommit

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
 import React, { Component } from 'react'
 
-// redux imports
-import store from './redux/store'
-import {addArticle} from './redux/actions'
-
 export default class App extends Component {
   
   render() {
     
-    // dispatches the addArticle action
-    store.dispatch(addArticle({
-      title: "This is a new article"
-    }))
-    
     return (
       <div className="App">  
+      <h1>Hello</h1>
       </div>
     )
   }
