90024e0c7c4277e33892cc29109602b8d2798749
Daniel Kwok <danielkwok@gpsfleet.my>
2019-12-03 17:33:39 +0800

    download file from buffer working

--- src/App.js
+++ src/App.js
@@ -1,20 +1,48 @@
 import React, { Component } from 'react'
 
-// redux imports
-import store from './redux/store'
-import {addArticle} from './redux/actions'
-
 export default class App extends Component {
-  
+
+  state = {
+    data: ''
+  }
+
+  componentDidMount() {
+    const url = `https://mrquwrjkri.execute-api.ap-southeast-1.amazonaws.com/v1/transitRecord/timeFrame/pdf`
+    const body = {
+      vid: "vhc#fd9bed20-85b5-11e9-89f5-adf28913c0aa",
+      startTime: 1573169072000,
+      endTime: 1573269072000,
+      status: ['PARKING', 'MOVING', 'OVERSPEED', 'IDLING']
+    }
+    console.log('fetching...')
+
+    fetch(url, {
+      method: 'POST',
+      headers: {
+        'Accept': '*/*',
+        'Content-Type': 'application/pdf charset=utf-8',
+        
+      },
+      body: JSON.stringify(body)
+    })
+    .then(response=>response.json())
+    .then(json=>{
+      console.log({json})
+      const buffer = Buffer.from(json.buffer.data)
+      const file = new Blob([buffer], {type: 'application/pdf'})
+      const fileURL = URL.createObjectURL(file)
+
+      const fileLink = document.createElement('a')
+      fileLink.href = fileURL
+      fileLink.download = json.name
+      fileLink.click()
+    })
+  }
+
   render() {
-    
-    // dispatches the addArticle action
-    store.dispatch(addArticle({
-      title: "This is a new article"
-    }))
-    
     return (
-      <div className="App">  
+      <div className="App">
+        {JSON.stringify(this.state.data)}
       </div>
     )
   }
